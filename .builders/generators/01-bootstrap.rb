20823829d7ef9a79bb86075ca21f5d0f437fc8da
David Cruwys <david@ideasmen.com.au>
2022-01-16 14:00:39 +1100

    first commit

--- .builders/generators/01-bootstrap.rb
+++ .builders/generators/01-bootstrap.rb
@@ -11,12 +11,11 @@ KManager.action :bootstrap do
       )
       .github do
         parent.options.repo_info = repo_info
-        delete_repository # (name: :xmen, organization: 'klueless-webpack5-samples')
-        sleep 2; open_repository # (name: :xmen, organization: 'klueless-webpack5-samples')
-        create_repository # (name: :xmen, organization: 'klueless-webpack5-samples')
-        sleep 2; open_repository # (name: :xmen, organization: 'klueless-webpack5-samples')
-        # list_repositories
+        # delete_repository # (name: :xmen, organization: 'klueless-webpack5-samples')
         # sleep 2; open_repository # (name: :xmen, organization: 'klueless-webpack5-samples')
+        create_repository # (name: :xmen, organization: 'klueless-webpack5-samples')
+        # sleep 2; open_repository # (name: :xmen, organization: 'klueless-webpack5-samples')
+        # list_repositories
       end
       .blueprint(
         name: :bin_hook,
